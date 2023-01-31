7e22890600e9d61d77a7222beff8bea71b5a011b
jscottransom <james.scottran@gmail.com>
2023-01-31 13:55:21 -0800

    build deck basics

--- ultimate_holdem/python/ultimate_holdem/render.py
+++ ultimate_holdem/python/ultimate_holdem/render.py
@@ -1,3 +1,23 @@
 import ultimate_holdem
 
-print(ultimate_holdem.sum_as_string(5,2))
+
+def build_deck() -> list:
+    """
+    Builds a standard deck of cards used for gameplay.
+
+    Args:
+        None
+
+    Returns:
+        list: A list of Card objects represeenting playable values
+    """
+    number_cards = ultimate_holdem.build_number_cards()
+    face_cards = ultimate_holdem.build_face_cards()
+
+    return [number_cards + face_cards]
+
+if __name__ == '__main__':
+    deck = build_deck()
+    print(deck)
+   
+
--- ultimate_holdem/src/lib.rs
+++ ultimate_holdem/src/lib.rs
@@ -1,14 +1,79 @@
 use pyo3::prelude::*;
+use pyo3;
+use std::vec::Vec;
 
-/// Formats the sum of two numbers as string.
+
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+pub fn build_number_cards() -> Vec<Card> {
+    let mut cards = Vec::new();
+
+    let suits = vec!["Spades", "Hearts", "Clubs", "Diamonds"];
+    for cardsuit in suits {
+        for n in 2..10 {
+
+            let number_string = n.to_string();
+            let borrowed_number_string: &str = &number_string;
+            let mut card = Card {
+                suit: cardsuit.to_string(),
+                value: borrowed_number_string.to_string()
+            };
+
+            cards.push(card);
+        }
+    }
+
+    cards
+}
+
+#[pyfunction]
+pub fn build_face_cards() -> Vec<Card> {
+    let mut cards = Vec::new();
+
+    let suits = vec!["Spades", "Hearts", "Clubs", "Diamonds"];
+    let faces = vec!["Ace", "King", "Queen", "King"];
+    for cardsuit in suits {
+        for face in &faces {
+
+            let mut card = Card {
+                suit: cardsuit.to_string(),
+                value: face.to_string()
+            };
+
+            cards.push(card);
+        }
+    }
+
+    cards
+}
+
+
+#[pyclass]
+pub struct Card {
+    suit: String,
+    value: String
+}
+
+#[pymethods]
+impl Card {
+    #[new]
+    fn new(cardsuit: String, cardvalue: String) -> Self {
+        Card {
+            suit: cardsuit,
+            value: cardvalue
+        }
+    }
+
+    fn __repr__(&self) -> String {
+        // Allows for printing to stdout
+        format!("Suit: {}, Value: {}", self.suit, self.value)
+    }
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn ultimate_holdem(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<Card>()?;
+    m.add_function(wrap_pyfunction!(build_number_cards, m)?)?;
+    m.add_function(wrap_pyfunction!(build_face_cards, m)?)?;
     Ok(())
 }
